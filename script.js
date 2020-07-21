c158a3a7c14102aec49824a6c1004d11d31c8487
Irma Georgia <irmamodzg@gmail.com>
2020-07-21 02:24:10 -0400

    change style

---
+++ script.js
@@ -0,0 +1,8 @@
+function sidebar() {
+  document.getElementById("sidebar").classList.toggle("active");
+  document.getElementById("main").classList.toggle("active");
+
+  console.log(document.getElementById("sidebar").classList.toggle("sidebar"));
+
+  // ;
+}
